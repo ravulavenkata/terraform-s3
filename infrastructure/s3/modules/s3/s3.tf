fd6c9a8df13489bcf1a8a2e732e423a65dbdd6b0
Venkata Krishna Ravula <ravulavenkata@gmail.com>
2018-09-18 10:29:37 -0400

    Update s3.tf

--- infrastructure/s3/modules/s3/s3.tf
+++ infrastructure/s3/modules/s3/s3.tf
@@ -4,13 +4,69 @@ variable "AWS_REGION" {}
 variable "AWS_BACKUP_REGION" {}
 
 provider "aws" {
-  region = "eu-west-1"
+  
 }
 
 provider "aws" {
-  alias  = "central"
-  region = "eu-central-1"
+alias = "oregon"
+region = "us-west-2"
 }
+
+provider "aws" {
+alias="virginia"
+region="us-east-1"
+}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AWS_BACK_REGION" {
+  default = "us-west-1"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "tf-test-bucket-12345-venkat-test"
+ provider = "aws.virginia"
+ acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+ tags {
+    Name        = "Primary"
+    Environment = "Dev"
+  }
+  replication_configuration {
+    role = "${aws_iam_role.replication.arn}"
+
+    rules {
+      prefix = ""
+      status = "Enabled"
+
+      destination {
+        bucket        = "${aws_s3_bucket.bucket2.arn}"
+        storage_class = "STANDARD"
+      }
+    }
+  }
+ 
+ }
+
+ resource "aws_s3_bucket" "bucket2" {
+  bucket = "tf-test-bucket-12345-venkat-test-backup"
+ provider = "aws.oregon"
+ acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+ tags {
+    Name        = "Duplicate"
+    Environment = "Dev"
+  }
+ }
+
 
 resource "aws_iam_role" "replication" {
   name = "tf-iam-role-replication-12345"
@@ -41,23 +97,31 @@ resource "aws_iam_policy" "replication" {
   "Statement": [
     {
       "Action": [
-        "s3:*"
+        "s3:GetReplicationConfiguration",
+        "s3:ListBucket"
       ],
       "Effect": "Allow",
       "Resource": [
-        "${aws_s3_bucket.bucket.arn}",
+        "${aws_s3_bucket.bucket.arn}"
+      ]
+    },
+    {
+      "Action": [
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionAcl"
+      ],
+      "Effect": "Allow",
+      "Resource": [
         "${aws_s3_bucket.bucket.arn}/*"
       ]
-    },    
+    },
     {
       "Action": [
         "s3:ReplicateObject",
-        "s3:ReplicateDelete",
-        "s3:ReplicateTags",
-        "s3:GetObjectVersionTagging"
+        "s3:ReplicateDelete"
       ],
       "Effect": "Allow",
-      "Resource": "${aws_s3_bucket.destination.arn}/*"
+      "Resource": "${aws_s3_bucket.bucket2.arn}/*"
     }
   ]
 }
@@ -65,41 +129,7 @@ POLICY
 }
 
 resource "aws_iam_policy_attachment" "replication" {
-  name       = "tf-iam-role-attachment-replication-12345-venkat-backup"
+  name       = "tf-iam-role-attachment-replication-12345"
   roles      = ["${aws_iam_role.replication.name}"]
   policy_arn = "${aws_iam_policy.replication.arn}"
-}
-
-resource "aws_s3_bucket" "destination" {
-  bucket   = "tf-test-bucket-destination-12345-venkat"
-  region   = "eu-west-1"
-
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_s3_bucket" "bucket" {
-  provider = "aws.central"
-  bucket   = "tf-test-bucket-12345-venkat"
-  acl      = "private"
-  region   = "eu-central-1"
-
-  versioning {
-    enabled = true
-  }
-
-  replication_configuration {
-    role = "${aws_iam_role.replication.arn}"
-
-    rules {
-      prefix = ""
-      status = "Enabled"
-
-      destination {
-        bucket        = "${aws_s3_bucket.destination.arn}"
-        storage_class = "STANDARD"
-      }
-    }
-  }
 }
